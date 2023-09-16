97b97abad4a7642e3d3a674377f6de4b31135035
Eugene <distinctao1@gmail.com>
2023-09-16 17:34:18 +0900

    HTML connect with JS for login page

--- js/script_login.js
+++ js/script_login.js
@@ -1,19 +1,43 @@
-const login_form = document.getElementById("login_form");
-const loginBt = document.getElementById("loginBt");
-const loginErrorMsg = document.getElementById("errorMsg");
+let inputBox = document.querySelectorAll('.inputBox');
+
+let inputId = document.getElementById('inputId');
+let inputPw = document.getElementById('inputPw');
+let loginBt = document.getElementById('loginBt');
+let idLength = inputId.value.length;
+let loginErrorMsg = document.getElementById('errorMsg');
+
+inputBox.forEach((item, index) => {
+  let inputText = inputBox[index].firstElementChild;
+  inputText.addEventListener('keyup', e => {
+    let changeInputBgColor = () => {
+      let inputBgColor = e.target.parentNode.style; 
+      if (e.target.value.length > 0) {
+        inputBgColor.backgroundColor = "#fff";
+        if (inputId.value.includes('') && inputPw.value.length >= 8) {
+          loginBt.disabled = false;
+        }
+      } else {
+        inputBgColor.backgroundColor = '#efefef';
+        loginBt.disabled = true;
+      }
+    }
+    changeInputBgColor();
+  });
+});
 
 loginBt.addEventListener("click", (event) => {
   event.preventDefault();
-  const userId = login_form.inputId.value;
-  const userPw = login_form.inputPw.value;
-  let username = "distincta@gmail.com"
-  let password = 1111
-
-    if (username === "userId" && password === "userPw") {
-        alert("You have successfully logged in.");
-        location("13_4_index.html");
+  let loginForm = document.getElementById("loginForm")
+  const userId = loginForm.inputId.value;
+  const userPw = loginForm.inputPw.value;
+  let username = "distinctao";
+  let password = "11111111";
+  if (username === userId && password === userPw) {
+    alert("You have successfully logged in.");
+    location.href="/13_4_index.html";
     } else {
       loginErrorMsg.style.opacity = 1;
+      changeInputBgColor();
     }
   }
 );
